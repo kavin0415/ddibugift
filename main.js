14f2d9d545d48c952ba745a69478d23233682607
sseng mo <kavinking@naver.com>
2023-01-29 21:10:41 +0900

    firrrst commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = new URL("http://localhost:3000" + _url).searchParams;
